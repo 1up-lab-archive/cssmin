e6356d673f0608b17af27430b88e580288c14ba7
joe.scylla@gmail.com <joe.scylla@gmail.com@2273e29d-6945-0410-8e99-033b4a520b20>
2011-07-13 07:09:28 +0000

    Fixed Issue #37
    git-svn-id: http://cssmin.googlecode.com/svn/trunk@154 2273e29d-6945-0410-8e99-033b4a520b20

--- source/minifier/plugins/CssConvertRgbColorsMinifierPlugin.php
+++ source/minifier/plugins/CssConvertRgbColorsMinifierPlugin.php
@@ -35,7 +35,7 @@ class CssConvertRgbColorsMinifierPlugin extends aCssMinifierPlugin
 	 */
 	public function apply(aCssToken &$token)
 		{
-		if (stripos($token->Value, "hsl") !== false && preg_match($this->reMatch, $token->Value, $m))
+		if (stripos($token->Value, "rgb") !== false && preg_match($this->reMatch, $token->Value, $m))
 			{
 			for ($i = 1, $l = count($m); $i < $l; $i++)
 				{
