3a2f8f87c80f11300af533eb9a28ee297a466437
Jim Schmid <jim.schmid@1up.io>
2013-12-20 14:52:43 +0100

    Added basic test suite.

---
+++ tests/Oneup/CssMin/TestSuiteLoaderTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Oneup\CssMin;
+
+class TestSuiteLoaderTest extends \PHPUnit_Framework_TestCase
+{
+    public function testIfTestSuiteLoads()
+    {
+        $this->assertTrue(true);
+    }
+}
